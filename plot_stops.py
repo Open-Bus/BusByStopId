0a31d45047e3b7e4671f2c040acccffbac6fb3f0
First <ragavendra.bn@gmail.com>
2022-05-25 18:20:42 -0700

    read and print stops info

--- plot_stops.py
+++ plot_stops.py
@@ -1,3 +1,17 @@
-file = open("static/stops.txt", "r")
-print(file.readline())
-print(file.readline())
+
+
+def read_stops(filepath):
+    file = open(filepath, "r")
+
+    for line in file:
+        #arr = line
+        stop = line.split(",")
+        print("Stop id: " + stop[0])
+        print("Stop name: " + stop[2])
+        print("Latitude: " + stop[4])
+        print("Longitude: " + stop[5])
+        print()
+
+    file.close()
+
+read_stops("static/stops.txt")
